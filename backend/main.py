034ebc8f31107acce2d9c60c10da6dfb2326310b
melumilic <melumilic@gmail.com>
2023-01-29 09:28:43 -0800

    add page to render database

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, redirect, url_for, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import requests
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
@@ -9,9 +10,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ingredients.db"
 
 db = SQLAlchemy(app)
 
+logged_in = True
+current_user = "testuser"
+bootstrap = Bootstrap5(app)
+
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), unique=True, nullable=False)
+    username = db.Column(db.String(32), unique=False, nullable=False)
     ingredient_name = db.Column(db.String(250), unique=False, nullable=False)
     expiration = db.Column(db.String(8),unique=False,nullable=False)
     img_url = db.Column(db.String(500))
@@ -25,7 +30,17 @@ class Ingredient(db.Model):
 @app.route("/")
 def home():
     db.create_all()
-    return "Nothing Here"
+    if logged_in:
+        username = current_user
+        # ingredients = db.session.execute(db.select(Ingredient).filter_by(username=username))
+        ingredients = db.session.query(Ingredient).filter_by(username=username).all()
+        if(ingredients==None):
+            return 404
+        ingredient_list = [row.to_dict() for row in ingredients]
+        return render_template("index.html",ingredient_list=ingredient_list)        
+    else:
+        return "Login to begin adding ingredients"
+    
 
 
 @app.route("/ingredient/add", methods=["POST"])
