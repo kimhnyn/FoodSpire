2b3c3d001932a39ab5e8a2077da0eedfe9b85258
Kimberly <kimhnguyeno@g.ucla.edu>
2023-01-29 09:33:46 -0800

    commit final

--- foodspire/src/App.js
+++ foodspire/src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
+import React from 'react';
+import Header from "./components/Header.js";
+import Home from './pages/home.jsx';
+import Pages from './pages/pages.jsx';
+import "./App.css";
+{/*import AddIngredient from "./AddIngredient";
+import SeeDates from "./SeeDates";
+import IngredientList from "./IngredientList";*/}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <Home />
+      <Pages />
+      {/*<AddIngredient />
+      <IngredientList />
+  <SeeDates />*/}
+    </div>  
+  )
+};
 
 export default App;
